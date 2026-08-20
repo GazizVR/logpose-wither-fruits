4bc2e6862ef464d55fcc73ec7e2a2593ec69ede1
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-08-20 12:40:38 +0500

    feat(UsersManager): additianal StatusEffects

--- src/main/kotlin/me/gaziz/logpose/witherfruits/UsersManager.kt
+++ src/main/kotlin/me/gaziz/logpose/witherfruits/UsersManager.kt
@@ -1,28 +1,27 @@
 package me.gaziz.logpose.witherfruits
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
+import net.minecraft.entity.effect.StatusEffect
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.registry.entry.RegistryEntry
 
 object UsersManager {
-    private val effectEntry = StatusEffects.SLOWNESS
-    private val statusEffect = StatusEffectInstance(
-        effectEntry,
-        StatusEffectInstance.INFINITE,
-        StatusEffectInstance.MAX_AMPLIFIER,
-        false,
-        false,
-        false,
-
-    )
-    private val secondEffect = StatusEffects.BLINDNESS
-    private val secondStatusEffect = StatusEffectInstance(
-        secondEffect,
-        StatusEffectInstance.INFINITE,
-        1,
-        false,
-        false,
-        false,
+    private val negativeEffect: (RegistryEntry<StatusEffect>) -> StatusEffectInstance = {
+        StatusEffectInstance(
+            it,
+            StatusEffectInstance.INFINITE,
+            StatusEffectInstance.MAX_AMPLIFIER,
+            false,
+            false,
+            false
+        )
+    }
+    private val negativeEffects = listOf(
+        negativeEffect(StatusEffects.SLOWNESS),
+        negativeEffect(StatusEffects.BLINDNESS),
+        negativeEffect(StatusEffects.MINING_FATIGUE),
+        negativeEffect(StatusEffects.WEAKNESS),
     )
     private var hasNegativeEffects = false
     fun initialize(){
@@ -31,14 +30,17 @@ object UsersManager {
             server.playerManager.playerList.forEach { player ->
                 if(state.getFruits().containsKey(player.uuidAsString)) {
                     if(player.isTouchingWater) {
+                        player.setJumping(false)
                         hasNegativeEffects = true
-                        player.addStatusEffect(statusEffect)
-                        player.addStatusEffect(secondStatusEffect)
+                        negativeEffects.forEach {
+                            player.addStatusEffect(it)
+                        }
                     } else {
                         if(hasNegativeEffects) {
                             hasNegativeEffects = false
-                            player.removeStatusEffect(effectEntry)
-                            player.removeStatusEffect(secondEffect)
+                            negativeEffects.forEach {
+                                player.removeStatusEffect(it.effectType)
+                            }
                         }
                     }
                 }
