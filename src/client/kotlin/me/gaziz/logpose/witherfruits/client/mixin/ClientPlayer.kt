bb70dee0715f175e793b7ce0a1471c6ef50b8bc1
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-08-21 11:04:27 +0500

    feat(client): LivingEntity.swimUpward method mixin and remove custom attribute modifier GENERIC_GRAVITY from main

---
+++ src/client/kotlin/me/gaziz/logpose/witherfruits/client/mixin/ClientPlayer.kt
@@ -0,0 +1,19 @@
+package me.gaziz.logpose.witherfruits.client.mixin
+
+import net.minecraft.entity.LivingEntity
+import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.injection.At
+import org.spongepowered.asm.mixin.injection.Inject
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+
+@Mixin(LivingEntity::class)
+class ClientPlayer {
+    @Inject(
+        method = ["swimUpward"],
+        at = [At("HEAD")],
+        cancellable = true
+    )
+    fun onSwimUpward(ci: CallbackInfo) {
+        ci.cancel()
+    }
+}
