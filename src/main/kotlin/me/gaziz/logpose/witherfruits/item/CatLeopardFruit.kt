83448910ff83c79672cf6a43e544c4da5487e7d3
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-08-23 20:11:10 +0500

    feat(CatLeopardFruit): add attributes and effects after transformation to full form

--- src/main/kotlin/me/gaziz/logpose/witherfruits/item/CatLeopardFruit.kt
+++ src/main/kotlin/me/gaziz/logpose/witherfruits/item/CatLeopardFruit.kt
@@ -10,7 +10,6 @@ import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
 import net.minecraft.registry.entry.RegistryEntry
 import net.minecraft.server.network.ServerPlayerEntity
-import net.minecraft.text.Text
 
 class CatLeopardFruit: ZoanFruit("cat_leopard_fruit") {
     override var currentForm: Form = Form.Base
@@ -27,35 +26,60 @@ class CatLeopardFruit: ZoanFruit("cat_leopard_fruit") {
             false
         )
     }
-    private val transformEffects = listOf(
-        createEffect(StatusEffects.NIGHT_VISION),
-        createEffect(StatusEffects.RESISTANCE)
-    )
-    private val transformModifiers = mapOf(
-        EntityAttributes.PLAYER_SNEAKING_SPEED to EntityAttributeModifier(
-            Initializer.id("leopard_sneaking_speed"),
-            0.2,
+    private fun createModifier(
+        attr: RegistryEntry<EntityAttribute>,
+        value: Double
+    ): Pair<RegistryEntry<EntityAttribute>, EntityAttributeModifier> {
+        val path = attr.idAsString.substringAfterLast(".")
+        return attr to EntityAttributeModifier(
+            Initializer.id("leopard_$path").withPrefixedPath("attribute"),
+            value,
             EntityAttributeModifier.Operation.ADD_VALUE
         )
-    )
-    private fun updateModifier(
+    }
+    private fun setModifier(
         entity: LivingEntity,
         modifier: Pair<RegistryEntry<EntityAttribute>, EntityAttributeModifier>
     ) {
         val attr = entity.getAttributeInstance(modifier.first)
         if(attr?.hasModifier(modifier.second.id) == false) {
             attr.addPersistentModifier(modifier.second)
-        } else {
-            attr?.removeModifier(modifier.second)
         }
     }
+    private fun removeModifier(
+        entity: LivingEntity,
+        modifier: Pair<RegistryEntry<EntityAttribute>, EntityAttributeModifier>
+    ) {
+        val attr = entity.getAttributeInstance(modifier.first)
+        attr?.removeModifier(modifier.second)
+    }
+    private val transformEffects = listOf(
+        createEffect(StatusEffects.NIGHT_VISION),
+        createEffect(StatusEffects.HUNGER,1),
+    )
+    private val transformModifiers = mapOf(
+        createModifier(EntityAttributes.GENERIC_SCALE,-0.34),
+        createModifier(EntityAttributes.PLAYER_BLOCK_BREAK_SPEED,-1.0),
+        createModifier(EntityAttributes.GENERIC_ARMOR,4.0),
+        //Damage
+        createModifier(EntityAttributes.GENERIC_ATTACK_DAMAGE,3.0),
+        createModifier(EntityAttributes.GENERIC_ATTACK_KNOCKBACK,-0.5),
+        //Movement
+        createModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED,0.04),
+        createModifier(EntityAttributes.PLAYER_SNEAKING_SPEED,0.3),
+        createModifier(EntityAttributes.GENERIC_STEP_HEIGHT,0.4),
+        //Jump
+        createModifier(EntityAttributes.GENERIC_JUMP_STRENGTH,0.2),
+        createModifier(EntityAttributes.GENERIC_SAFE_FALL_DISTANCE,2.5),
+        createModifier(EntityAttributes.GENERIC_FALL_DAMAGE_MULTIPLIER,-0.2),
+    )
     override fun transform(user: LivingEntity) {
         currentForm = Form.Full
         transformEffects.forEach {
             user.addStatusEffect(it)
         }
         transformModifiers.forEach { (k, v) ->
-            updateModifier(user,k to v)
+            setModifier(user,k to v)
         }
     }
     override fun hybridTransform(user: LivingEntity) {
@@ -65,7 +89,7 @@ class CatLeopardFruit: ZoanFruit("cat_leopard_fruit") {
         when(currentForm) {
             Form.Full -> {
                 transformModifiers.forEach { (k, v) ->
-                    updateModifier(user,k to v)
+                    removeModifier(user,k to v)
                 }
                 transformEffects.forEach {
                     user.removeStatusEffect(it.effectType)
@@ -79,10 +103,10 @@ class CatLeopardFruit: ZoanFruit("cat_leopard_fruit") {
     fun toggleTransform(
         player: ServerPlayerEntity
     ) {
-        if(currentForm == Form.Full) baseTransform(player) else transform(player)
-        player.sendMessage(
-            Text.literal(currentForm.name),
-            true
-        )
+        if(currentForm == Form.Full){
+            baseTransform(player)
+        } else {
+            transform(player)
+        }
     }
 }
