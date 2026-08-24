721483f8333890e5748184f769f9092d28fe3b62
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-08-24 13:27:11 +0500

    fix(modifier): remove redundant modifiers from DebuffManager

--- src/main/kotlin/me/gaziz/logpose/witherfruits/modifier/DebuffManager.kt
+++ src/main/kotlin/me/gaziz/logpose/witherfruits/modifier/DebuffManager.kt
@@ -4,8 +4,6 @@ import me.gaziz.logpose.witherfruits.PersistFruitsState
 import me.gaziz.logpose.witherfruits.network.NetworkManager
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
-import net.minecraft.entity.attribute.EntityAttribute
-import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.effect.StatusEffect
 import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.effect.StatusEffects
@@ -15,19 +13,7 @@ object DebuffManager {
     private val effect: (RegistryEntry<StatusEffect>) -> StatusEffectInstance = {
         createEffect(it, StatusEffectInstance.MAX_AMPLIFIER)
     }
-    private val effects = listOf(
-        effect(StatusEffects.BLINDNESS),
-        effect(StatusEffects.WEAKNESS),
-    )
-    private var hasEffects = false
-    private val modifier: (RegistryEntry<EntityAttribute>) -> Modifier = {
-        val prefix = "debuff_manager"
-        createModifier(it,-1.0,prefix)
-    }
-    private val modifiers = listOf(
-        modifier(EntityAttributes.GENERIC_STEP_HEIGHT),
-        modifier(EntityAttributes.PLAYER_BLOCK_BREAK_SPEED),
-    )
+    private val effects = listOf(effect(StatusEffects.WEAKNESS))
     fun initialize(){
         ServerPlayConnectionEvents.JOIN.register { handler, _, server ->
             val player = handler.player
@@ -42,23 +28,9 @@ object DebuffManager {
                 if(playerFruit != null) {
                     if(player.isTouchingWater) {
                         player.setJumping(false)
-                        hasEffects = true
                         effects.forEach { player.addStatusEffect(it) }
-                        modifiers.forEach {
-                            val attr = player.getAttributeInstance(it.attribute)
-                            if(attr?.hasModifier(it.modifier.id) == false){
-                                attr.addPersistentModifier(it.modifier)
-                            }
-                        }
                     } else {
-                        if(hasEffects) {
-                            hasEffects = false
-                            effects.forEach { player.removeStatusEffect(it.effectType) }
-                            modifiers.forEach {
-                                val attr = player.getAttributeInstance(it.attribute)
-                                attr?.removeModifier(it.modifier)
-                            }
-                        }
+                        effects.forEach { player.removeStatusEffect(it.effectType) }
                     }
                 }
             }
