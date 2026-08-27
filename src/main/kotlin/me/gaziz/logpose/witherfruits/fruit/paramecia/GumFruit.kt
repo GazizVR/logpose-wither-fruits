4a41750375bad873bf8e303d22599122cc08892b
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-08-27 20:42:41 +0500

    feat(fruit): GumFruit buffs

--- src/main/kotlin/me/gaziz/logpose/witherfruits/fruit/paramecia/GumFruit.kt
+++ src/main/kotlin/me/gaziz/logpose/witherfruits/fruit/paramecia/GumFruit.kt
@@ -1,3 +1,33 @@
 package me.gaziz.logpose.witherfruits.fruit.paramecia
 
-class GumFruit: ParameciaFruit("gum_fruit")
+import me.gaziz.logpose.witherfruits.modifier.Modifier
+import me.gaziz.logpose.witherfruits.modifier.createEffect
+import me.gaziz.logpose.witherfruits.modifier.createModifier
+import net.minecraft.entity.attribute.EntityAttribute
+import net.minecraft.entity.attribute.EntityAttributes
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.registry.entry.RegistryEntry
+import net.minecraft.registry.tag.DamageTypeTags
+
+class GumFruit: ParameciaFruit("gum_fruit") {
+    override val immuneDamageTypes = listOf(
+        DamageTypeTags.IS_LIGHTNING,
+        DamageTypeTags.IS_FALL,
+        DamageTypeTags.IS_EXPLOSION
+    )
+    override val buffEffects: List<StatusEffectInstance> = listOf(
+        createEffect(StatusEffects.JUMP_BOOST,1)
+    )
+    private fun createModifier(
+        attr: RegistryEntry<EntityAttribute>,
+        value: Double,
+    ): Modifier {
+        val prefix = "gum"
+        return createModifier(attr, value, prefix)
+    }
+    override val modifiers: List<Modifier> = listOf(
+        createModifier(EntityAttributes.PLAYER_BLOCK_INTERACTION_RANGE,1.6),
+        createModifier(EntityAttributes.PLAYER_ENTITY_INTERACTION_RANGE,2.0),
+    )
+}
--- src/main/kotlin/me/gaziz/logpose/witherfruits/fruit/paramecia/ParameciaFruit.kt
+++ src/main/kotlin/me/gaziz/logpose/witherfruits/fruit/paramecia/ParameciaFruit.kt
@@ -1,7 +1,15 @@
 package me.gaziz.logpose.witherfruits.fruit.paramecia
 
+import me.gaziz.logpose.witherfruits.PersistFruitsState
+import me.gaziz.logpose.witherfruits.PlayerManager
 import me.gaziz.logpose.witherfruits.fruit.ModFruits
 import me.gaziz.logpose.witherfruits.fruit.WitherFruit
+import me.gaziz.logpose.witherfruits.modifier.BuffManager
+import me.gaziz.logpose.witherfruits.modifier.Modifier
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
+import net.minecraft.entity.damage.DamageType
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.registry.tag.TagKey
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.Rarity
 
@@ -10,5 +18,34 @@ abstract class ParameciaFruit(path: String): WitherFruit(
     rarity = Rarity.UNCOMMON,
     tooltipKey = ModFruits.parameciaTooltipKey
 ) {
-    override fun onRemove(user: ServerPlayerEntity) {}
+    abstract val immuneDamageTypes: List<TagKey<DamageType>>
+    abstract val buffEffects: List<StatusEffectInstance>
+    abstract val modifiers: List<Modifier>
+
+    init {
+        ServerLifecycleEvents.SERVER_STARTED.register {
+            val state = PersistFruitsState().getPersistFruitsState(it)
+            state.getFruits().forEach { (key, value) ->
+                if(value == this) {
+                    BuffManager.setEffects(key,buffEffects)
+                    PlayerManager.setDamageTypes(key,immuneDamageTypes)
+                    BuffManager.setModifiers(key,modifiers)
+                }
+            }
+        }
+    }
+
+    override fun onRemove(user: ServerPlayerEntity) {
+        val key = user.uuidAsString
+        PlayerManager.removeDamageTypes(key)
+        BuffManager.removeModifiers(user)
+        BuffManager.removeEffects(user)
+    }
+
+    override fun onEat(user: ServerPlayerEntity) {
+        val key = user.uuidAsString
+        BuffManager.setEffects(key,buffEffects)
+        PlayerManager.setDamageTypes(key,immuneDamageTypes)
+        BuffManager.setModifiers(key,modifiers)
+    }
 }
