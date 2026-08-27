9920b4f489dc9d2240ef258c124b3c325b869d41
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-08-27 18:07:37 +0500

    feat(fruit): FlameFruit global cooldown varible

--- src/main/kotlin/me/gaziz/logpose/witherfruits/fruit/logia/FlameFruit.kt
+++ src/main/kotlin/me/gaziz/logpose/witherfruits/fruit/logia/FlameFruit.kt
@@ -10,6 +10,13 @@ import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
 
 class FlameFruit: LogiaFruit("flame_fruit") {
+    override fun onRemove(user: ServerPlayerEntity) {
+        super.onRemove(user)
+        val key = user.uuidAsString
+        globalCooldown.remove(key)
+        firstCooldown.remove(key)
+        secondCooldown.remove(key)
+    }
     override val buffEffects = listOf(
         createEffect(StatusEffects.FIRE_RESISTANCE)
     )
@@ -21,15 +28,23 @@ class FlameFruit: LogiaFruit("flame_fruit") {
     )
 
     private val cooldownText = Text.literal("On cooldown")
+    private val globalCooldown = mutableMapOf<String,Int>()
+
     private val firstCooldown = mutableMapOf<String, Int>()
     override fun firstAbility(user: ServerPlayerEntity){
-        val cooldown = firstCooldown[user.uuidAsString] ?: 0
+        val key = user.uuidAsString
         val currentTick = user.server.ticks
+
+        val globalCD = globalCooldown[key] ?: 0
+        if(currentTick < globalCD) return
+
+        val cooldown = firstCooldown[key] ?: 0
         if(currentTick < cooldown) {
             user.sendMessage(cooldownText,true)
             return
         }
-        firstCooldown[user.uuidAsString] = currentTick + (SharedConstants.TICKS_PER_SECOND*3)
+        globalCooldown[key] = currentTick + (SharedConstants.TICKS_PER_SECOND/2)
+        firstCooldown[key] = currentTick + (SharedConstants.TICKS_PER_SECOND*3)
         val smallFireball = SmallFireballEntity(
             user.world,
             user,
@@ -41,13 +56,19 @@ class FlameFruit: LogiaFruit("flame_fruit") {
 
     private val secondCooldown = mutableMapOf<String, Int>()
     override fun secondAbility(user: ServerPlayerEntity) {
-        val cooldown = secondCooldown[user.uuidAsString] ?: 0
+        val key = user.uuidAsString
         val currentTick = user.server.ticks
+
+        val globalCD = globalCooldown[key] ?: 0
+        if(currentTick < globalCD) return
+
+        val cooldown = secondCooldown[key] ?: 0
         if(currentTick < cooldown) {
             user.sendMessage(cooldownText,true)
             return
         }
-        secondCooldown[user.uuidAsString] = currentTick + (SharedConstants.TICKS_PER_SECOND*6)
+        globalCooldown[key] = currentTick + (SharedConstants.TICKS_PER_SECOND/2)
+        secondCooldown[key] = currentTick + (SharedConstants.TICKS_PER_SECOND*6)
         val fireball = FireballEntity(
             user.world,
             user,
