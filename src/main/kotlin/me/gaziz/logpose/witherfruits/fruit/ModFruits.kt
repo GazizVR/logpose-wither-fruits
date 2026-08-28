851fd1b9a2be84bbff1d6164c285dedfc5518e7c
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-08-28 14:31:42 +0500

    feat(fruit): LootTable Modify

--- src/main/kotlin/me/gaziz/logpose/witherfruits/fruit/ModFruits.kt
+++ src/main/kotlin/me/gaziz/logpose/witherfruits/fruit/ModFruits.kt
@@ -6,8 +6,14 @@ import me.gaziz.logpose.witherfruits.fruit.paramecia.GumFruit
 import me.gaziz.logpose.witherfruits.fruit.zoan.CatLeopardFruit
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.loot.LootPool
+import net.minecraft.loot.LootTables
+import net.minecraft.loot.condition.RandomChanceLootCondition
+import net.minecraft.loot.entry.ItemEntry
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
@@ -59,5 +65,25 @@ object ModFruits {
         register(gumFruit.id, gumFruit)
         register(catLeopardFruit.id, catLeopardFruit)
         register(flameFruit.id, flameFruit)
+        LootTableEvents.MODIFY.register { key, builder, source, lookup ->
+            if(key == LootTables.BURIED_TREASURE_CHEST) {
+                val poolBuilder = LootPool.builder()
+                    .rolls(ConstantLootNumberProvider.create(1f))
+                    .conditionally(RandomChanceLootCondition.builder(0.25f))
+                    .with(ItemEntry.builder(gumFruit).weight(1))
+                    .with(ItemEntry.builder(flameFruit).weight(2))
+                    .with(ItemEntry.builder(catLeopardFruit).weight(3))
+                builder.pool(poolBuilder)
+            }
+            if(key == LootTables.SHIPWRECK_TREASURE_CHEST) {
+                val poolBuilder = LootPool.builder()
+                    .rolls(ConstantLootNumberProvider.create(1f))
+                    .conditionally(RandomChanceLootCondition.builder(0.1f))
+                    .with(ItemEntry.builder(gumFruit).weight(1))
+                    .with(ItemEntry.builder(flameFruit).weight(2))
+                    .with(ItemEntry.builder(catLeopardFruit).weight(3))
+                builder.pool(poolBuilder)
+            }
+        }
     }
 }
